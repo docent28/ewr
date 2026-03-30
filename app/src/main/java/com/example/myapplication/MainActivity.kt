7b251328f087b6d541e9ed161b3d648ab721e67b
a.evladov <as@jkjhf.ru>
2026-03-30 20:00:42 +0300

    Добавил переменную

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val d = 6
 
     }
 }
